12e1f256b1410e68bc913859157d5d6cb58c3579
megawatts68 <andrew.f.watts@gmail.com>
2020-09-17 03:57:53 +0700

    Surprising progress. 2 of 3 functions done.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,30 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  let { durability, enhancement} = item
+  if (enhancement === 20) {
+    enhancement = 20
+  } else {
+    enhancement++
+  }
+  return { ...item,
+    enhancement, durability };
 }
 
 function fail(item) {
+  let { durability, enhancement} = item
+  if (enhancement < 15) {
+    durability = durability - 5
+  } else if (enhancement > 14) {
+    durability = durability - 10
+  
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item,
+    durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,52 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancer', () => {
+    describe('repair(item)', () => {
+        it('should return an item object', function() {
+            const item = {
+                name: "Whatever",
+                durability: 50,
+                enhancement: 1
+            }
+            expect(typeof enhancer.repair(item)).toBe(typeof item)
+        })
+        it('durability should be 100', function() {
+            const item = {
+                name: "Whatever",
+                durability: 50,
+                enhancement: 1
+            }
+            expect(enhancer.repair(item)).toEqual({name: "Whatever",
+        durability: 100, enhancement: 1})
+        })
+    })
+    describe('success(item)', () => {
+        it('should increase enhancement by 1, unless enhancement is 20', function() {
+            const item = {
+                name: "Whatever",
+                durability: 50,
+                enhancement: 1
+            }
+            expect(enhancer.success(item).enhancement).toBe(2)
+        })
+        it('should increase enhancement by 1, unless enhancement is 20', function() {
+            const item = {
+                name: "Whatever",
+                durability: 50,
+                enhancement: 20
+            }
+            expect(enhancer.success(item).enhancement).toBe(20)
+        })
+        it('should not change durability', function() {
+            const item = {
+                name: "Whatever",
+                durability: 50,
+                enhancement: 20
+            }
+            expect(enhancer.success(item).durability).toBe(50)
+        })
+    })
+    describe('fail(item)', () => {
+        
+    })
+})
